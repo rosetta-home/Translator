0f1b2c644c8602f480fd97c0f580940fffdd0dee
John O'Sullivan <josullivan1@luc.edu>
2017-06-27 16:03:46 -0500

    Added Comments

--- README.md
+++ README.md
@@ -2,26 +2,3 @@
 
 [Preact]: https://github.com/developit/preact
 [webpack]: https://webpack.github.io
-
-/*start() {
-  var config = {
-
-  };
-  const nodeID = "0000000081474d35";
-  const endpoint = "wss://open-data.api.satori.com";
-  const appKey = "da4F19eb331E6465a6C206DE6c9cE2dc";
-  const channel = "rosetta-home";
-
-  var rm = new RosettaHome(nodeID,config);
-  var rtm = new satori_sdk(endpoint, appKey);
-  rtm.on("enter-connected", function() { console.log("Connected to rosetta-home via satori.js!"); });
-  var subscription = rtm.subscribe("where", satori_sdk.SubscriptionMode.SIMPLE, {
-    filter: 'SELECT * FROM `rosetta-home` WHERE tags.node_id=\"'+ nodeID +'\"',
-  });
-  subscription.on('rtm/subscription/data', function (pdu) {
-    pdu.body.messages.forEach(function (msg) {
-      rm.process(msg);
-    });
-  });
-  rtm.start();
-}*/
--- src/store.js
+++ src/store.js
@@ -1,6 +1,8 @@
 import { createStore } from 'redux';
 /* Array of the hex colors for RH  */
 const colors = ['#0277bd','#03a9f4','#81d4fa','#ffcc80','#ff9800','#ef6c00'];
+/* How many minutes graphs will display at once */
+const graph_minutes = 0.5;
 /* Update type tracker so the incoming item from satori know what graphs to be send too. */
 function updateTypeTracker(tracker,id,ts,node) {
 	/* Creates a copy of the tracker */
@@ -30,18 +32,30 @@ function createDefautGraph(ts) {
 }
 /* Processes the data that is coming in from the Satori websocket */
 function processData(graph,typetracker,item) {
+	/* Creates a copy of the graph */
 	var newgraph = JSON.parse(JSON.stringify(graph));
+	/* Checks if the item type exists in the type tracker, hence needing to be added to graphs */
 	if (typetracker[item.measurement] !== undefined) {
+		/* Gets the tracking data from the type tracking object */
 		var data = typetracker[item.measurement];
+		/* Goes through each object to check if the nodeID are the same and if so adds the item */
 		for (var i = 0, j = data.length; i < j; i++) {
+			/* Gets the nodeID */
 			var node_id = data[i].nodeID;
+			/* Compares the nodeID with the nodeID in the item from websocket */
 			if (node_id === item['tags'].node_id) {
+				/* If so gets the current graph datum */
 				var current_datum = newgraph[data[i].graphID].datum;
+				/* Goes through the datum set to find the set to add item to values */
 				for (var x = 0, y = current_datum.length; x < y; x++) {
+					/* Gets the current set */
 					var cd = current_datum[x];
-					if (cd.key === item.measurement) {
+					/* Checks the set type and item measurement */
+					if (cd.type === item.measurement) {
+						/* Gets the timestamp and creates the lastItem */
 						var ms = item.timestamp / 1000000;
 						var lastItem = { x:ms,y:item.fields.value };
+						/* Adds the lastItem to the data set and sets it as the lastItem in graph*/
 						cd.values.push(lastItem);
 						cd.lastItem = lastItem;
 					}
@@ -52,21 +66,32 @@ function processData(graph,typetracker,item) {
 	return newgraph;
 }
 function addpoint(graph,id) {
+	/* Creates a copy of the graph object */
 	var copygraph = JSON.parse(JSON.stringify(graph));
+	/* Gets the current datum using the id */
 	var graph_datum = copygraph[id].datum;
+	/* Goes through the sets in the current datum */
 	for (var x = 0, y = graph_datum.length; x < y; x++) {
+		/* Gets the current set from the datum */
 		var current_set = graph_datum[x];
+		/* Will only add point from the timer if the lastItem is not null and values count is not 0 */
 		if ((current_set.lastItem !== null) && (current_set.values !== 0)) {
+			 /* Gets the lastItem */
 			 var newlastItem = current_set.lastItem;
+			 /* Adds a second to the timestamp */
 			 newlastItem.x = newlastItem.x + 1000;
+			 /* Sets the new lastItem and adds to dataset to update graph */
 			 current_set.lastItem = newlastItem;
 			 current_set.values.push(newlastItem);
-			 if (current_set.values.length > 20) {
-				 var diff = current_set.values.length - 20;
+			 /* Checks if the data set values in the current datum is more than the graph_minutes count */
+			 if (current_set.values.length > (graph_minutes * 60)) {
+				 /* If so it splice the array to give a nice transition */
+				 var diff = current_set.values.length - (graph_minutes * 60);
 				 current_set.values.splice(0,diff);
 			 }
 		}
 	}
+	/* Returns the new graph state */
 	return copygraph;
 }
 
